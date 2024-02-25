402169036088a84311dee40afd2ae32aa4f182df
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-02-25 23:52:55 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, f;
+    char d;
+
+    cin >> f >> d >> x >> d >> y;
+
+    cout << 0 << f << "-" << y <<"-" << x;
+    return 0;
+}
