57f048d2171f97e3db30dcd2ea4572e75e812324
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-02-25 22:52:10 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240225/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, d, y;
+    char c;
+
+    cin >> m >> c >> d >> c >> y;
+
+    cout << y << "." << m << "." << d;
+    return 0;
+}
