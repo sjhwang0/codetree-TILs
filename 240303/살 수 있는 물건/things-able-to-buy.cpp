f14516c5e7ded96ec78cb51d024dfe9b24e88f3e
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 00:03:11 +0900

    Title: 살 수 있는 물건, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/살 수 있는 물건/things-able-to-buy.cpp
+++ 240303/살 수 있는 물건/things-able-to-buy.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
 
     if(n >= 3000) cout << "book";
-    else if(1000 <= n < 3000) cout << "mask";
+    else if(n >= 1000) cout << "mask";
     else cout << "no";
 
     return 0;
