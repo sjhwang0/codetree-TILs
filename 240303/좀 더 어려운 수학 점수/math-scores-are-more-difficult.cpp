907c4757090c1611f3d836f3734061a8b6f263f4
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 02:12:13 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240303/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int math_a, eng_a, math_b, eng_b;
+
+    cin >> math_a >> eng_a >> math_b >> eng_b;
+
+    if(math_a >= math_b && eng_a > eng_b) cout << 'A';
+    else cout << 'B';
+
+    return 0;
+}
