8acfe7694b8188c12dc212a227c06141be3d8f01
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 14:56:03 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int i = n;
+
+    while(i >= 1){
+        cout << i << " ";
+        i--;
+    }
+    return 0;
+}
