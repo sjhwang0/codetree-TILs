cd5b657dd799910a99d2124717ff3fc7e9c78652
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 14:50:31 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+
+    cin >> n;
+
+    i = 1;
+    while(i <= n){
+        cout << "*" << endl;
+        i++;
+    }
+    return 0;
+}
