fc1dc964e69a8bdfbeac3988efc7f625f5077508
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 14:42:55 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+
+    cin >> a >> b;
+
+    for(i = a;i <= b;i++){
+        if(i % 2 == 1) cout << i << " ";
+    }
+
+    return 0;
+}
