30e8786f2fa81f354b451d0b6b72c18285fb1775
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 13:48:02 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/윤년인가/is-leap-year.cpp
+++ 240303/윤년인가/is-leap-year.cpp
@@ -6,8 +6,8 @@ int main() {
 
     cin >> y;
 
-    if(y % 4 == 0 || y % 100 == 0 && y % 400 != 0) cout << "true";
-    else cout << "false";
-    
+    if(y % 4 != 0 || y % 100 == 0 && y % 400 != 0) cout << "false";
+    else cout << "true";
+
     return 0;
 }
