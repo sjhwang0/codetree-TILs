ea53671682ca6c9fbb90d44c77e447fe094610f3
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-03 14:57:25 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240303/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+
+    cin >> b >> a;
+
+    i = b;
+
+    while(i >= a){
+        cout << i << " ";
+        i -= 2;
+    }
+    return 0;
+}
