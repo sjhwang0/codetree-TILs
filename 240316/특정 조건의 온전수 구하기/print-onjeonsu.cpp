4dc3e4724164c1ccfeff98245e32eefadb7c4328
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-16 13:06:00 +0900

    Title: 특정 조건의 온전수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240316/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int i = 1;i <= n;i++){
+        if(i % 2 == 0) continue;
+        else if(i % 10 == 5) continue;
+        else if(i % 3 == 0 && i % 9 != 0) continue;
+        else cout << i << " ";
+    }
+
+    return 0;
+}
