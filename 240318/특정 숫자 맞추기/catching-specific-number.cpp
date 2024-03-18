1615bcbd3775638ef555ac087a5a6d963f1b2d17
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-18 12:27:49 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    while(true){
+        cin >> n;
+        if(n < 25) cout << "Higher" << endl;
+        else if(n > 25) cout << "Lower" << endl;
+        else{
+            cout << "Good";
+            break;
+        }
+    }
+    return 0;
+}
