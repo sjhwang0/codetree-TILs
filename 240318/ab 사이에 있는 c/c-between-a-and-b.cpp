9b895e500021dd13d21eab114ecc9f07733265d4
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-18 13:37:39 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    bool satisfied = false;
+
+    cin >> a >> b >> c;
+
+    for(int i = a;i <= b;i++){
+        if(i % c == 0) satisfied = true;
+    } 
+
+    if(satisfied) cout << "YES";
+    else cout << "NO";
+    
+    return 0;
+}
