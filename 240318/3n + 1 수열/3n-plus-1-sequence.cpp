7835ddd45dbaa0c15165304fddfab65f5a3bf5e1
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-18 13:00:39 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240318/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+
+    cin >> n;
+
+    while(true){
+        if(n == 1) break;
+        
+        if(n % 2 == 0){
+            n /= 2;
+            cnt++;
+        }
+        else{
+            n = n * 3 + 1;
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
