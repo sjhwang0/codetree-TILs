be8ff3262ab7243daf976c26dfacbcf8c61ec4cc
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-20 19:02:39 +0900

    Title: 직각 삼각형 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 240320/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -4,19 +4,13 @@ using namespace std;
 
 int main() {
     int n;
-
     cin >> n;
 
-    for(int i = 1;i <= 2 * n - 1;i++){
-        if(i % 2 == 0){
-            continue;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= 2 * i - 1;j++){
+            cout << "*";
         }
-        else{
-            for(int j = 1;j <= i;j++){
-                cout << "*";
-            }
-            cout << endl;
-        }
+        cout << endl;
     }
     return 0;
 }
