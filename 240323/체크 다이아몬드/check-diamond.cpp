9a6a0dbf57ac7c1ba2ee41547abaf1ca091ba8c8
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-23 17:03:02 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/체크 다이아몬드/check-diamond.cpp
+++ 240323/체크 다이아몬드/check-diamond.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n;
 
     for(int i = 0;i <n;i++){
-        for(int j = n - i - 1;j > 0;j--){
+        for(int j = 0;j < n - i - 1;j++){
             cout << " ";
         }
         for(int j = 0;j <= i;j++){
@@ -14,11 +14,11 @@ int main() {
         }
         cout << endl;
     }
-    for(int i = 0;i < n-1;i++){
-        for(int j = 0;j <= i;j++){
+    for(int i = n - 2;i >= 0;i--){
+        for(int j = 0;j < n - i - 1;j++){
             cout << " ";
         }
-        for(int j = n - i - 1;j > 0;j--){
+        for(int j = 0;j <= i;j++){
             cout << "* ";
         }
         cout << endl;
