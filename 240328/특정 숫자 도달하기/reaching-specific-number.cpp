a1677120edcc607f9eb4288d56a7d124b844db45
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-28 17:29:19 +0900

    Title: 특정 숫자 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240328/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240328/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 
 int main() {
-    int a[10], sum = 0;
-
-    cout << fixed;
-    cout.precision(1);
+    int a[10], sum = 0, cnt = 0;
 
     for(int i = 0;i < 10;i++){
         cin >> a[i];
-        if(a[i] >= 250){
-            cout << sum << " " << (double)sum / i;
-            break;
-        }
-        sum += a[i];
-        if(i == 9) cout << sum << " " << (double)sum / 10;
     }
+
+    for(int i = 0;i < 10;i++){
+        if(a[i] >= 250) break;
+        sum += a[i];
+        cnt++;
+    }
+
+    double avg = (double)sum / cnt;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << avg;
+    
     return 0;
 }
