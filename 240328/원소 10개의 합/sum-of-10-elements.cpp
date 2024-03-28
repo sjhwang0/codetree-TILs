9736710d8059643d1e384950a4aad7f64524bef4
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-03-28 16:57:24 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10], sum = 0;
+    
+    for(int i = 0;i < 10;i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum;
+    return 0;
+}
