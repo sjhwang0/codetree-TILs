48b75091b11236a20f556d39856d20028ed9331f
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-01 10:38:23 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240401/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10], sum = 0;
+
+    for(int i = 0;i < 10;i++){
+        cin >> arr[i];
+    }
+    sum = arr[2] + arr[4] + arr[9];
+    cout << sum;
+    return 0;
+}
