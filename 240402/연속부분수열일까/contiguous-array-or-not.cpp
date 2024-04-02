f11f7c2c80d918e8f5304c0ed2e8a43e3a51a768
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-02 16:47:35 +0900

    Title: 연속부분수열일까, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240402/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240402/연속부분수열일까/contiguous-array-or-not.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    vector<int> b(m);
+    int n1, n2;
+    cin >> n1 >> n2;
+    vector<int> a(n1);
+    vector<int> b(n2);
 
-    for(int i = 0; i<n; i++) cin >> a[i];
-    for(int i = 0; i<m; i++) cin >> b[i];
+    for(int i = 0;i < n1;i++) cin >> a[i];
+    for(int i = 0;i < n2;i++) cin >> b[i];
 
     bool yes = false;
-    for(int i = 0; i<n-m+1; i++){
+    for(int i = 0;i < n1-n2+1;i++){
         bool satisfied = true;
-        for(int j = 0; j<m; j++){
-            if(a[i+j] != b[j]) satisfied = false;
+        for(int j = 0;j < n2;j++){
+            if(a[i+j] != b[j]) satisfied = false; 
         }
         if(satisfied) yes = true;
     }
