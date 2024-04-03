03194c6587e81302d5535063c1a99acc265e1500
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-03 22:50:12 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240403/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 240403/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int n, arr[1000], count_value[1001], max_value = 0;
+    int n, arr[1000], count_value[1001] = {}, max_value = 0;
     cin >> n;
 
-    for(int i = 0;i < n + 1;i++){
+    for(int i = 0;i < n;i++){
         cin >> arr[i];
         count_value[arr[i]]++;
     }
-    for(int i = 1;i < n + 1;i++){
+    for(int i = 1;i < 1001;i++){
         if(count_value[i] == 1 && i > max_value) max_value = i; 
     }
 
