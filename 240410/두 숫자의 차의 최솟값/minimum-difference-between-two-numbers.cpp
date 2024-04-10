4c55e3e898463041bd57431a5ef4c439d2a24eb6
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-10 13:41:36 +0900

    Title: 두 숫자의 차의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[10], min = 100;
+    cin >> n;
+
+    for(int i = 0;i < n;i++)
+        cin >> arr[i];
+
+    for(int i = 0;i < n - 1;i++){
+        if(arr[i+1] - arr[i] < min) min = arr[i+1] - arr[i];
+    }
+    
+    cout << min;
+    return 0;
+}
