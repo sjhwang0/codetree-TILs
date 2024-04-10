01aa823822f584da83c0727d2a98e57e016edb2c
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-10 21:48:14 +0900

    Title: 대각선으로 숫자 채우기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 240410/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -5,32 +5,23 @@ int main(){
     int n, m;
     cin >> n >> m;
 
-    int arr[100][100];
+    int arr[100][100] = {};
     int num = 1;
 
-    for(int start_col = 0;start_col < m;start_col++){
-        int curr_row = 0;
-        int curr_col = start_col;
+    for(int row = 0;row < n;row++){
+        for(int col = 0;col < m;col++){
+            if(arr[row][col] == 0){
+                int curr_col = col;
+                int curr_row = row;
 
-        while(curr_col >= 0 && curr_row < n){
-            arr[curr_row][curr_col] = num;
+                while(curr_col >= 0 && curr_row < n){
+                    arr[curr_row][curr_col] = num;
 
-            curr_row++;
-            curr_col--;
-            num++;
-        }
-    }
-    
-    for(int start_row = 1;start_row < n;start_row++){
-        int curr_col = m - 1;
-        int curr_row = start_row;
-
-        while(curr_col >= 0 && curr_row < n){
-            arr[curr_row][curr_col] = num;
-
-            curr_row++;
-            curr_col--;
-            num++;
+                    curr_row++;
+                    curr_col--;
+                    num++;
+                }
+            }
         }
     }
 
@@ -39,6 +30,5 @@ int main(){
             cout << arr[row][col] << " ";
         cout << "\n";
     }
-
     return 0;
 }
