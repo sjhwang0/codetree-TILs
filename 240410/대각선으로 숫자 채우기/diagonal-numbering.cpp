0f7cffd333e9e3aa630816de9f7013499cefca74
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-10 21:44:19 +0900

    Title: 대각선으로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/대각선으로 숫자 채우기/diagonal-numbering.cpp
+++ 240410/대각선으로 숫자 채우기/diagonal-numbering.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-void fillDiagonal(int n, int m) {
-    vector<vector<int>> matrix(n, vector<int>(m, 0)); // n * m 크기의 2차원 벡터 생성
-
-    int num = 1;
-
-    for (int diag = 0; diag < n + m - 1; diag++) {
-        int start_row = max(0, diag - m + 1);
-        int count = min(diag + 1, min(n, m));
-        for (int i = 0; i < count; i++) {
-            int row = start_row + i;
-            int col = min(m, diag + 1) - i - 1;
-            if (row < n && col < m) // 유효한 인덱스인 경우에만 값을 할당
-                matrix[row][col] = num++;
-        }
-    }
-
-    // 출력
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-int main() {
+int main(){
     int n, m;
     cin >> n >> m;
-    fillDiagonal(n, m);
+
+    int arr[100][100];
+    int num = 1;
+
+    for(int start_col = 0;start_col < m;start_col++){
+        int curr_row = 0;
+        int curr_col = start_col;
+
+        while(curr_col >= 0 && curr_row < n){
+            arr[curr_row][curr_col] = num;
+
+            curr_row++;
+            curr_col--;
+            num++;
+        }
+    }
+    
+    for(int start_row = 1;start_row < n;start_row++){
+        int curr_col = m - 1;
+        int curr_row = start_row;
+
+        while(curr_col >= 0 && curr_row < n){
+            arr[curr_row][curr_col] = num;
+
+            curr_row++;
+            curr_col--;
+            num++;
+        }
+    }
+
+    for(int row = 0;row < n;row++){
+        for(int col = 0;col < m;col++)
+            cout << arr[row][col] << " ";
+        cout << "\n";
+    }
+
     return 0;
 }
