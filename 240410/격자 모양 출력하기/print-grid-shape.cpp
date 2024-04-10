fae08f48da410a2c334bb98fe98f2cd5448934e2
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-10 22:43:34 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int arr[11][11] = {};
+
+    for(int i = 0;i < m;i++){
+        int r, c;
+        cin >> r >> c;
+        arr[r][c] = r * c;
+    }
+
+    for(int i = 1;i < n + 1;i++){
+        for(int j = 1;j < n + 1;j++)
+            cout << arr[i][j] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
