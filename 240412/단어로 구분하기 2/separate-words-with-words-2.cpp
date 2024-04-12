c71145babd65000acf48ddfa8bd625c55d3b56a5
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-12 20:15:30 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string arr[10];
+
+    for(int i=  0;i < 10;i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0;i < 10;i++){
+        if(i % 2 == 0) cout << arr[i] << "\n";
+    }
+    return 0;
+}
