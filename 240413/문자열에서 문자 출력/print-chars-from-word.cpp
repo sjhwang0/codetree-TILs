33a501b89537dc78457d5f6fba6afea19d1a7c92
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-13 21:26:22 +0900

    Title: 문자열에서 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for(int i = 0;str[i] != '\0';i++){
+        cout << str[i] << "\n";
+    }
+    return 0;
+}
