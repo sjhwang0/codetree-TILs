af58d0c8ce7be284d0a0f8aa40fe58793185c799
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-21 15:38:43 +0900

    Title: 문자 제거하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240421/문자 제거하기/del-char.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int len = str.length();
+
+    while(len > 1){
+        int i;
+        cin >> i;
+
+        if(i > len)
+            str.erase(len - 1, 1);
+        else
+            str.erase(i, 1);
+        len--;
+
+        cout << str << "\n";
+    }
+    return 0;
+}
