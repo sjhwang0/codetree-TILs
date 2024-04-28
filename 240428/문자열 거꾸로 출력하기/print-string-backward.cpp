d9dec878381f779fbd4e8660f00f7a810a7fa957
SJ Hwang <57068041+sjhwang0@users.noreply.github.com>
2024-04-28 23:48:37 +0900

    Title: 문자열 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/문자열 거꾸로 출력하기/print-string-backward.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str;
+
+    while(1){
+        cin >> str;
+        if(str == "END") break;
+        reverse(str.begin(), str.end());
+        cout << str << "\n";
+    }
+    return 0;
+}
